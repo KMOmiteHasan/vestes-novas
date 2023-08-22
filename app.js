687caaaa83f5dfa06f4ad1ef7e945108263c495a
KMOmiteHasan <126360771+KMOmiteHasan@users.noreply.github.com>
2023-08-23 00:08:17 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -27,3 +27,104 @@ $(document).ready(function () {
         });
 });
 
+$(document).ready(function () {
+    $(".women-prod-listings")
+        .not(".slick-initialized")
+        .slick({
+            infinite: true,
+            dots: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            speed: 2000,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            prevArrow: ".women-slider-btn-left",
+            nextArrow: ".women-slider-btn-right",
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 520,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+});
+
+
+$(document).ready(function () {
+    $(".men-prod-listings")
+        .not(".slick-initialized")
+        .slick({
+            infinite: true,
+            dots: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            speed: 2000,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            prevArrow: ".men-slider-btn-left",
+            nextArrow: ".men-slider-btn-right",
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 520,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+});
+
+
+$(document).ready(function () {
+    $(".kids-prod-listings")
+        .not(".slick-initialized")
+        .slick({
+            infinite: true,
+            dots: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            speed: 2000,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            prevArrow: ".kids-slider-btn-left",
+            nextArrow: ".kids-slider-btn-right",
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 520,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+});
+
